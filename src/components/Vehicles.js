7c55a9d6b25d40a81ebefc60f474927438ff67b7
Piyush Yadav <work.piyush.yadav@gmail.com>
2019-09-28 15:40:22 +0530

    Filtering works as expected now

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -53,7 +53,7 @@ class Vehicles extends Component {
                     secondLine: 'Price: Rs.500/day',
                     btnName: 'Book Baleno',
                     location: '*',
-                    carType: 2
+                    carType: 3
                 },
                 {
                     src: 'https://imgd.aeplcdn.com/664x374/cw/ec/35463/Ford-Figo-Right-Front-Three-Quarter-151644.jpg?wm=0',
@@ -64,10 +64,10 @@ class Vehicles extends Component {
                     carType: 2
                 },
                 {
-                    src: 'https://imgd.aeplcdn.com/664x374/cw/ec/35463/Ford-Figo-Right-Front-Three-Quarter-151644.jpg?wm=0',
-                    firstLine: 'Ford Figo',
+                    src: 'https://auto.ndtvimg.com/car-images/colors/ford/ecosport/ford-ecosport-absolute-black.png',
+                    firstLine: 'Ford EcoSport',
                     secondLine: 'Price: Rs.500/day',
-                    btnName: 'Book Figo',
+                    btnName: 'Book EcoSport',
                     location: '*',
                     carType: 2
                 },
@@ -83,21 +83,33 @@ class Vehicles extends Component {
     
     render() {
         return (
-            <div className="row mt-5 offset-1 text-center">
-                <button className="btn rounded col-1 bg-info ml-3 mt-2" onClick={() => this.handleOnclick(1)}>Sedan</button>
-                <button className="btn rounded col-1 bg-info ml-3 mt-2" onClick={() => this.handleOnclick(2)}>SUV</button>
-                <button className="btn rounded col-1 bg-info ml-3 mt-2" onClick={() => this.handleOnclick(3)}>Hatchback</button>
-                {this.state.data.filter(element => element.carType === this.state.typeCheck).map((element, index) => {
-                    return (
-                        <div className="col-sm-1 col col-lg-3 mr-5 ">
-                            <VehicleCard info={element} index={index}/>
-                        </div>
-                    );
-                })}
-
+            <div className="row mt-5 ">
+                <div className="row col-3 mt-5 offset-4">
+                    <button className="btn rounded bg-dark text-light ml-3 mt-2" onClick={() => this.handleOnclick(1)}>Sedan</button>
+                    <button className="btn rounded bg-dark text-light ml-3 mt-2" onClick={() => this.handleOnclick(2)}>SUV</button>
+                    <button className="btn rounded bg-dark text-light ml-3 mt-2" onClick={() => this.handleOnclick(3)}>Hatchback</button>
+                </div>
+                <div className="row mt-5 offset-1 text-center">
+                    {/* {this.state.data.filter(element => element.carType >= this.state.typeCheck).map((element, index) => {
+                        return (
+                            <div className="col-sm-1 col col-lg-3 mr-5 ">
+                                <VehicleCard info={element} index={index}/>
+                            </div>
+                        );
+                    })} */}
+                    {this.state.data.filter(element => (this.state.typeCheck === 0 ?  element : element.carType === this.state.typeCheck)).map((element, index) => {
+                        return (
+                            <div className="col-sm-1 col col-lg-3 mr-5 ">
+                                <VehicleCard info={element} index={index}/>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         );
     }
 }
 
 export default Vehicles;
+
+
